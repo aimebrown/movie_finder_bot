f3679f2d45d928bb78d0dbd345c67775dbee918e
aimebrown <akakpobrown1@gmail.com>
2026-03-28 23:47:49 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 
 # Token of Telegram bot
-TOKEN = ""
+TOKEN = "8505559677:AAFGbPA9xwkQiq0jwWVEpSTk5eC9Kkfjye8"
 
 
 # API key of TMDB (v3 auth)
-API_KEY= ""
+API_KEY= "feb5dfc1d4d696d756b9130cd92f2fbf"
